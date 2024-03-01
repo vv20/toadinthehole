c9ebede4343be3a4350331d29d17b98713c64ad6
vv20 <victorvasilyev1806@gmail.com>
2024-03-01 11:53:40 +0000

    Inputting Amplify params as cmd args.

--- stack/create_amplify_config.py
+++ stack/create_amplify_config.py
@@ -8,20 +8,19 @@ from get_cognito_user_pool_web_client_id import \
     get_cognito_user_pool_web_client_id
 
 
-def create_amplify_config(environment):
-    region = os.getenv('AWS_REGION')
-    domain_name = os.getenv('DEPLOYMENT_DOMAIN_NAME')
-
+def create_amplify_config(environment, region, domain_name):
     with open('frontend/src/amplifyconfiguration.json.template', 'r') as template_file:
         template = json.load(template_file)
         template['Auth']['Cognito']['userPoolClientId'] = get_cognito_user_pool_web_client_id(environment)
         template['Auth']['Cognito']['userPoolId'] = get_cognito_user_pool_id(environment)
         template['API']['REST']['ToadInTheHoleAPI']['region'] = region
-        template['API']['REST']['ToadInTheHoleAPI']['endpoint'] = 'https://' + Domain.API.get_domain_name(environment, domain_name) + '/prod'
+        template['API']['REST']['ToadInTheHoleAPI']['endpoint'] = 'https://' + Domain.API.get_domain_name(environment, domain_name)
         with open('frontend/src/amplifyconfiguration.json', 'w') as output_file:
             print(json.dumps(template))
             json.dump(fp=output_file, obj=template)
 
 if __name__ == '__main__':
     environment = sys.argv[1]
-    create_amplify_config(environment)
+    region = sys.argv[2]
+    domain_name = sys.argv[3]
+    create_amplify_config(environment, region, domain_name)
