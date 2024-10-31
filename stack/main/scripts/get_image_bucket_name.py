992dfa2e559ba4da2b85d2a57bfddd02af1342f0
Victor Vasilyev <victorvasilyev1806@gmail.com>
2024-10-31 19:07:22 +0000

    Parameter change??

--- stack/main/scripts/get_image_bucket_name.py
+++ stack/main/scripts/get_image_bucket_name.py
@@ -8,7 +8,7 @@ from ..common import Component
 
 def get_image_bucket_name(environment):
     client = boto3.client('s3')
-    buckets = client.list_buckets(MaxResults=10)['Buckets']
+    buckets = client.list_buckets(MaxBuckets=10)['Buckets']
     bucket = [b for b in buckets if Component.IMAGE_BUCKET.get_component_name(environment) in b['Name']][0]
     return bucket['Name']
 
