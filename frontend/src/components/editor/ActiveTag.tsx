20a8333d0e38d00b80407dac12321dce49388563
Victor Vasilyev <victorvasilyev1806@gmail.com>
2025-03-30 02:02:08 +0100

    Removed unused import

--- frontend/src/components/editor/ActiveTag.tsx
+++ frontend/src/components/editor/ActiveTag.tsx
@@ -1,6 +1,4 @@
 import { Dispatch, SetStateAction } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faX } from "@fortawesome/free-solid-svg-icons";
 
 import { APIRecipePreview } from "../../api/APIModel";
 import { useAppSelector } from "../../redux/hooks";
